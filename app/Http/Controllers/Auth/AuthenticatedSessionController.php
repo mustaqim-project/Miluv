99461c78fac524f036558b2117382391a316d2c1
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-03-15 19:35:40 +0700

    asdf2342324

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -25,6 +25,8 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+
+        dd($request);
         $request->authenticate();
 
         $request->session()->regenerate();
