ba27516800c9f5c841d608a417b93a23200f0997
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-03-17 02:27:05 +0700

    asdfasd234

--- resources/views/auth/layout/header.blade.php
+++ resources/views/auth/layout/header.blade.php
@@ -50,29 +50,38 @@
         <nav class="navigation">
             <div class="container">
                 <div class="row">
-                    <div class="col-auto col-lg-6">
-                        <div class="logo-branding mt-1">
-                            <a class="navbar-brand d-xs-hidden"
-                                href="@if (Auth::check()) {{ route('timeline') }} @endif">
-                                <img src="{{ get_system_logo_favicon($system_light_logo, 'light') }}" height="35px"
-                                    class="max-width-250px d-xs-hidden" alt="logo" />
-                            </a>
-
-                            <a class="navbar-brand d-block"
-                                href="@if (Auth::check()) {{ route('timeline') }} @endif">
-                                <img src="{{ get_system_logo_favicon($system_light_logo, 'favicon') }}" height="35px"
-                                    class="max-width-250px d-hidden d-xs-show mt--5px" alt="logo" />
+                    <div class="col-lg-2 col-sm-4">
+                        <div class="logo-branding">
+                            <button class="d-lg-none" type="button" data-bs-toggle="offcanvas"
+                                data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                                <i class="fw-bold fa-solid fa-sliders-h"></i>
+                            </button>
+                            @php
+                                $system_light_logo = \App\Models\Setting::where('type', 'system_light_logo')->value(
+                                    'description',
+                                );
+                            @endphp
+                            <a class="navbar-brand mt-2" href="{{ route('timeline') }}">
+                                <img src="{{ get_system_logo_favicon($system_light_logo, 'light') }}"
+                                    class="logo_height_width" alt="logo" />
                             </a>
                         </div>
                     </div>
 
-                    <div class="col-auto col-lg-6 ms-auto">
-                        <div class="login-btns ms-5">
+                    <div class="col-auto col-lg-6 ms-auto text-end">
+                        <div class="login-btns" style="margin-right: 20px;">
                             <a href="{{ route('login') }}"
-                                class="btn @if (Route::currentRouteName() == 'login') active @endif">{{ __('Login') }}</a>
+                                class="btn @if (Route::currentRouteName() == 'login') active @endif"
+                                style="font-size: 14px; padding: 6px 12px; margin-left: 5px;">
+                                {{ __('Login') }}
+                            </a>
+
                             @if (get_settings('public_signup') == 1)
                                 <a href="{{ route('register') }}"
-                                    class="btn @if (Route::currentRouteName() == 'register') active @endif">{{ __('Sign up') }}</a>
+                                    class="btn @if (Route::currentRouteName() == 'register') active @endif"
+                                    style="font-size: 14px; padding: 6px 12px; margin-left: 5px;">
+                                    {{ __('Sign up') }}
+                                </a>
                             @endif
                         </div>
                     </div>
