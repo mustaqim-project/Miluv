f52ef6b8cd83dba8ac3ee6003e8f65fe17940f64
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-03-16 10:12:23 +0700

    asdf2134

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,44 +24,34 @@
 
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-            
-                       
+                    
                         <div class="form-group form-email">
-                            <label for="#">{{get_phrase('Email')}}</label>
-                            <input type="email" name="email" value="{{ old('email') }}" placeholder="{{get_phrase('Enter your email address')}}">
+                            <label for="email">{{ get_phrase('Email') }}</label>
+                            <input type="email" name="email" id="email" value="{{ old('email') }}" placeholder="{{ get_phrase('Enter your email address') }}">
                         </div>
                         <p class="text-danger">{{ $errors->first('email') }}</p>
-                        <div class="form-group form-pass">
-                            <label for="#">{{get_phrase('Password')}}</label>
-                            <input type="password" name="password" placeholder="{{get_phrase('Your password')}}">
+                    
+                        <div class="form-group form-pass" style="position: relative;">
+                            <label for="password">{{ get_phrase('Password') }}</label>
+                            <input type="password" name="password" id="password" placeholder="{{ get_phrase('Your password') }}">
+                                <i id="togglePassword" class="fas fa-eye"></i>
                         </div>
-            
+                    
                         <!-- Remember Me -->
                         <div class="mb-3 form-check">
                             <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
                             <label class="form-check-label" for="remember_me">{{ get_phrase('Remember me') }}</label>
-                          </div>
-
+                        </div>
+                    
                         <input class="btn btn-primary my-3" type="submit" name="submit" id="submit" value="Log In">
-
-                        
+                    
                         <div class="flex items-center justify-end mt-2">
                             @if (Route::has('password.request'))
-                                <a class="" href="{{ route('password.request') }}">
+                                <a href="{{ route('password.request') }}">
                                     {{ get_phrase('Forgot your password?') }}
                                 </a>
                             @endif
                         </div>
-
-                        <!-- <div class="row mt-4">
-                            <div class="col-md-6 pb-2">
-                                <a href="javascript:;" onclick="$('[name=email]').val('admin@example.com'); $('[name=password]').val('12345678');" class="btn btn-outline-primary w-100">Login as Admin</a>
-                            </div>
-                            <div class="col-md-6 pb-2">
-                                <a href="javascript:;" onclick="$('[name=email]').val('karenjrios@example.com'); $('[name=password]').val('12345678');" class="btn btn-outline-primary w-100">Login as General user</a>
-                            </div>
-                        </div> -->
-            
                     </form>
 
                 </div>
