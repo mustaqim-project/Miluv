a1fe361918b302c94fd1ac751c0c7d8c2e222fa1
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-03-16 22:24:18 +0700

    asdfas123

--- resources/views/frontend/general/single-blog.blade.php
+++ resources/views/frontend/general/single-blog.blade.php
@@ -130,8 +130,45 @@
 
 <body class="{{ $theme_color }}">
 
-    @include('auth.layout.header')
+    @php $system_light_logo = \App\Models\Setting::where('type', 'system_light_logo')->value('description'); @endphp
 
+    <!-- header -->
+    <header class="header header-default py-3">
+        <nav class="navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="col-auto col-lg-6">
+                        <div class="logo-branding mt-1">
+                            <a class="navbar-brand d-xs-hidden"
+                                href="@if (Auth::check()) {{ route('timeline') }} @endif">
+                                <img src="{{ get_system_logo_favicon($system_light_logo, 'light') }}" height="35px"
+                                    class="max-width-250px d-xs-hidden" alt="logo" />
+                            </a>
+
+                            <a class="navbar-brand d-block"
+                                href="@if (Auth::check()) {{ route('timeline') }} @endif">
+                                <img src="{{ get_system_logo_favicon($system_light_logo, 'favicon') }}" height="35px"
+                                    class="max-width-250px d-hidden d-xs-show mt--5px" alt="logo" />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-auto col-lg-6 ms-auto">
+                        <div class="login-btns ms-5">
+                            <a href="{{ route('login') }}"
+                                class="btn @if (Route::currentRouteName() == 'login') active @endif">{{ __('Login') }}</a>
+                            @if (get_settings('public_signup') == 1)
+                                <a href="{{ route('register') }}"
+                                    class="btn @if (Route::currentRouteName() == 'register') active @endif">{{ __('Sign up') }}</a>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    
     <main class="main my-4 mt-12">
         <div class="container">
             <div class="row">
