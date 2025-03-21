34927387b0e080d51f9e26be1fdae28a8442cbb6
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-03-21 16:01:14 +0700

    asdfasd234

--- resources/views/frontend/master/layouts/menu.blade.php
+++ resources/views/frontend/master/layouts/menu.blade.php
@@ -1,18 +1,18 @@
 <div class="menubar-area footer-fixed">
     <div class="toolbar-inner menubar-nav">
-        <a href="home.html" class="nav-link active">
+        <a href="#" class="nav-link active">
         <i class="flaticon flaticon-dog-house"></i>
         </a>
-        <a href="explore.html" class="nav-link">
+        <a href="#" class="nav-link">
         <i class="flaticon flaticon-search"></i>
         </a>
-        <a href="wishlist.html" class="nav-link">
+        <a href="#" class="nav-link">
         <i class="flaticon flaticon-heart"></i>
         </a>
-        <a href="chat-list.html" class="nav-link">
+        <a href="#" class="nav-link">
         <i class="flaticon flaticon-chat-1"></i>
         </a>
-        <a href="profile.html" class="nav-link">
+        <a href="#" class="nav-link">
         <i class="flaticon flaticon-user"></i>
         </a>
     </div>
