d335de2d1302a8cf4065c3bd3a95bfd861b6fda3
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-03-21 10:07:27 +0700

    sdfgsdf354

--- resources/views/frontend/chat/chat.blade.php
+++ resources/views/frontend/chat/chat.blade.php
@@ -1,24 +1,23 @@
 <div class="message-box chat_control bg-white border radius-8">
     @if (!empty($reciver_data))
         <div class="modal-header d-flex bg-secondary">
-
             <div class="avatar d-flex">
                 <a href="#" class="d-flex align-items-center">
                     <div class="avatar avatar-lg me-2">
-                        <img src="{{ get_user_image($reciver_data->photo, 'optimized') }}" class="rounded-circle h-45"
-                            alt="">
+                        <img src="{{ get_user_image($reciver_data->photo, 'optimized') }}" class="rounded-circle h-45" alt="">
                         @if ($reciver_data->isOnline())
                             <span class="online-status active"></span>
                         @endif
                     </div>
                     <div class="name">
                         <h4 class="m-0 h6">{{ $reciver_data->name }}</h4>
-                        @if ($reciver_data->isOnline())
-                            <small class="d-block">{{ get_phrase('Active now') }}</small>
-                        @else
-                            <small class="d-block">
-                                {{ \Carbon\Carbon::parse($reciver_data->lastActive)->diffForHumans() }}</small>
-                        @endif
+                        <small class="d-block">
+                            @if ($reciver_data->isOnline())
+                                {{ get_phrase('Active now') }}
+                            @else
+                                {{ \Carbon\Carbon::parse($reciver_data->lastActive)->diffForHumans() }}
+                            @endif
+                        </small>
                     </div>
                 </a>
             </div>
@@ -27,11 +26,12 @@
                     <i class="fa-solid fa-ellipsis-vertical"></i>
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a class="dropdown-item" href="{{ route('user.profile.view', $reciver_data->id) }}"><i
-                                class="fa fa-user"></i>
-                            {{ get_phrase('View Profile') }}</a></li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('user.profile.view', $reciver_data->id) }}">
+                            <i class="fa fa-user"></i> {{ get_phrase('View Profile') }}
+                        </a>
+                    </li>
                 </ul>
-
             </div>
         </div>
         <div class="modal-body">
@@ -40,65 +40,39 @@
                     @include('frontend.chat.single-message')
                 </div>
             </div>
-
+        </div>
     @endif
-
-    @php
-        if (session()->has('product_ref_id')) {
-            $product_url = url('/') . '/product/view/' . session('product_ref_id');
-        }
-    @endphp
 
     <div class="mt-action">
         @if (!empty($reciver_data))
-            <!-- Chat textarea -->
-            <form class="ajaxForm" id="chatMessageFieldForm" action="{{ route('chat.save') }}" method="POST"
-                enctype="multipart/form-data">
+            <form class="ajaxForm" id="chatMessageFieldForm" action="{{ route('chat.save') }}" method="POST" enctype="multipart/form-data">
                 @csrf
-                <div class="nm_footer d-flex">
-
-                    <div class="d-flex w-100 message_b">
-                        <input type="hidden" name="reciver_id" value="{{ $reciver_data->id }}" id="">
-                        @if ($product != null)
-                            <input type="hidden" name="product_id" value="{{ $product }}">
-                        @endif
-                        <input type="hidden" name="thumbsup" value="0" id="ChatthumbsUpInput">
-                        <input type="text" class="form-control mb-sm-0 mb-3 ms-1 " name="message"
-                            id="ChatmessageField" value="@if (isset($product_url) && $product_url != null) {{ $product_url }} @endif"
-                            placeholder="Type a message">
-                            <a href="javascript:void(0)" id="messgeImageUploader"><img
-                                src="{{ asset('assets/frontend/images/image-a.png') }}" alt=""></a>
-                        <button class="btn btn-primary send no-processing no-uploading" id="ChatsentButton"><i
-                                class="fa-solid fa-paper-plane"></i></button>
-                                
-                        {{-- <button type="submit" class="btn btn-primary  send  no_loading no-processing no-uploading"
-                            id="ChatthumbsUp"><i class="fa-solid fa-thumbs-up"></i> </button> --}}
-                    </div>
-
-                </div>
-                <button type="reset" id="messageResetBox"
-                    class="visibility-hidden">{{ get_phrase('Reset') }}</button>
-                <div class="mt-footer">
-                    <div class="input-images d-hidden  image-uploader_custom_css" id="messageFileUploder">
-                    </div>
+                <div class="nm_footer d-flex align-items-center p-2 border-top">
+                    <input type="hidden" name="reciver_id" value="{{ $reciver_data->id }}">
+                    @if ($product != null)
+                        <input type="hidden" name="product_id" value="{{ $product }}">
+                    @endif
+                    <input type="hidden" name="thumbsup" value="0" id="ChatthumbsUpInput">
                     
-
+                    <input type="text" class="form-control flex-grow-1 me-2" name="message" id="ChatmessageField" placeholder="Type a message">
+                    
+                    <a href="javascript:void(0)" id="messgeImageUploader" class="d-flex align-items-center me-2">
+                        <img src="{{ asset('assets/frontend/images/image-a.png') }}" alt="" style="width: 24px; height: 24px;">
+                    </a>
+                    
+                    <button class="btn btn-primary send no-processing no-uploading" id="ChatsentButton">
+                        <i class="fa-solid fa-paper-plane"></i>
+                    </button>
                 </div>
             </form>
-            <!-- Button -->
-            @php
-                Session::forget('product_ref_id');
-            @endphp
         @else
-            <div
-                style="width: 100%; height: 500px; display:flex; justify-content:center; align-items:center; font-size:20px;">
+            <div class="d-flex justify-content-center align-items-center" style="height: 500px; font-size: 20px;">
                 <p>{{ get_phrase('No Conversion Start!') }}</p>
             </div>
         @endif
     </div>
+</div>
 
-</div>
-</div>
 
 
 
@@ -245,7 +219,7 @@
                         // Append the new message to the message body
                         $('#message_body').append(response
                             .message
-                            ); // Assuming `response.message` contains the new message HTML
+                        ); // Assuming `response.message` contains the new message HTML
 
                         setTimeout(() => {
                             var elem = document.getElementById('messageShowDiv');
