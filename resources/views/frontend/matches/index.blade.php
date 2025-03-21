64892dab0ac6d32d04745deb478c993f78b41f00
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-03-21 13:28:18 +0700

    asdfasdf

--- resources/views/frontend/matches/index.blade.php
+++ resources/views/frontend/matches/index.blade.php
@@ -1,8 +1,8 @@
-<div class="page-wrap">
+<div class="page-wrap"> 
     <div class="blog-header nblog_header p-20 radius-8 mb-3">
         <h1 class="h3">{{ get_phrase('Matches') }}</h1>
     </div>
-    <div class="g-3 blog-cards mt-3">
+    <div class="row g-3 blog-cards mt-3"> <!-- Tambahkan row di sini -->
         @foreach ($add_friend as $friend)
             @php
                 $userId = auth()->id();
@@ -17,7 +17,7 @@
             @endphp
 
             @if (!$hasRequestSent && !$hasRequestReceived)
-                <div class="col-lg-4 col-md-4 col-6">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12"> <!-- Gunakan grid yang lebih fleksibel -->
                     <div class="card sugg-card p-0 box_shadow border-none suggest_p radius-8">
                         <a href="{{ route('user.profile.view', $friend->id) }}" class="thumbnail-110-106"
                             style="background-image: url('{{ get_user_image($friend->photo, 'optimized') }}')"></a>
