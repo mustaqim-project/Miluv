329b6f93de42db63a70db6fb216797a6bb6edcf8
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-03-25 13:54:58 +0700

    asdf234234

--- resources/views/frontend/master/layouts/menu.blade.php
+++ resources/views/frontend/master/layouts/menu.blade.php
@@ -29,22 +29,22 @@
     <div class="toolbar-inner menubar-nav">
         <a href="/" class="nav-link">
             <i class="fa-solid fa-user-friends"></i>
-            <span>Matches</span>
+            <span style="color:#000000">Matches</span>
         </a>
         <a href="timeline.html" class="nav-link" style="margin-right: 1.5em">
             <i class="fa-solid fa-globe"></i>
-            <span>Explore</span>
+            <span style="color:#000000">Explore</span>
         </a>
         <a href="index.html" class="floating-button">
             <i class="fa-solid fa-plus"></i>
         </a>
         <a href="blog.html" class="nav-link" style="margin-left: 1.5em">
             <i class="fa-solid fa-pen-to-square"></i>
-            <span>Article</span>
+            <span style="color:#000000">Article</span>
         </a>
         <a href="settings.html" class="nav-link">
             <i class="fa-solid fa-user"></i>
-            <span>Profile</span>
+            <span style="color:#000000">Profile</span>
         </a>
     </div>
 </div>
